908aff772d7e88e6905750af9e65900109d64142
Yuyuan Lin <yuyuan.lin@yale.edu>
2022-02-21 11:00:15 -0800

    Finish shiny app development

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -11,8 +11,7 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Select a lab or vital measurement and a graphic type below.
-               The data was from MIMIC IV database"),
+      helpText("Select a lab or vital measurement."),
       
       selectInput(inputId = "var", label = "Lab/vital measurement",
                   choices = c("Heart rate" = 220045, 
@@ -34,21 +33,38 @@ ui <- fluidPage(
       
       br(),
       
+      helpText("Select a graphic type and whether comparing between 
+               different 30-day-mortality groups to generate a plot"),
+      
       selectInput(inputId = "geom", label = "Graphic Type",
                   choices = c("Histogram" = "hist",
                               "Density Plot" = "dens",
                               "Box Plot" = "boxp"),
                   multiple = FALSE),
       
+      checkboxInput(inputId = "comp", 
+                    "Compare groups devided by 30-day-mortality", 
+                    value = FALSE),
+      
       br(),
       
-      checkboxInput(inputId = "Compare", 
-                    "Compare groups devided by 30-day-mortality", value = FALSE)
+      helpText("Select a demographic variable to compare the measurement"),
+      
+      selectInput(inputId = "demo", label = "Demographic variable",
+                  choices = c("Gender" = "gender",
+                              "Ethinicity" = "ethnicity",
+                              "Language" = "language",
+                              "Insurance" = "insurance",
+                              "Marital Status" = "marital_status"),
+                  multiple = FALSE),
     ),
     
-    mainPanel(plotOutput(outputId = "plot"),
-              tableOutput(outputId = "test"),
-              textOutput(outputId = "test2"))
+    mainPanel(
+      tabsetPanel(
+        tabPanel(title = "Plot", plotOutput(outputId = "plot")),
+        tabPanel(title = "Table by demographic", tableOutput(outputId = "tble"))
+      )
+    )
   )
 )
 
@@ -56,13 +72,24 @@ ui <- fluidPage(
 # Server logic ----
 server <- function(input, output) {
   
+  # interactive data for plot ----
   data_plot <- reactive({
     var_name <- str_c("itemid_", input$var)
     icu_cohort %>% 
       select(c(var_name, "thirty_day_mort")) %>%
-      mutate(thirty_day_mort = as.factor(thirty_day_mort))
+      mutate(thirty_day_mort = as.factor(thirty_day_mort)) %>%
+      drop_na()
   })
   
+  # interactive data for table ----
+  data_table <- reactive({
+    var_name <- str_c("itemid_", input$var)
+    icu_cohort %>% 
+      select(c(var_name, input$demo)) %>%
+      drop_na()
+  })
+  
+  # plot output ----
   output$plot <- renderPlot({
     var_name <- str_c("itemid_", input$var)
     
@@ -71,25 +98,44 @@ server <- function(input, output) {
       labs(title = str_c("Histogram of ", var_name),
            x = var_name)
     
-    if (input$geom == "hist") {
-      plot <- plot + geom_histogram()
-    }
-    if (input$geom == "boxp") {
-      plot <- plot + geom_boxplot()
-    }
-    if (input$geom == "dens") {
-      plot <- plot + geom_density()
+    if (input$comp) {
+      if (input$geom == "hist") 
+        plot <- plot + geom_histogram(mapping = aes(fill = thirty_day_mort), 
+                                      position = "stack")
+      if (input$geom == "boxp") 
+        plot <- plot + geom_boxplot(mapping = aes(y = thirty_day_mort))
+      if (input$geom == "dens")
+        plot <- plot + geom_density(mapping = aes(fill = thirty_day_mort), 
+                                    alpha = 0.5)
+    }else{
+      if (input$geom == "hist") 
+        plot <- plot + geom_histogram(fill = "light blue")
+      if (input$geom == "boxp") 
+        plot <- plot + geom_boxplot()
+      if (input$geom == "dens")
+        plot <- plot + geom_density()
     }
     
     plot
+    
   })
   
-  output$test <- renderTable({
-    head(data_plot())
+  # table output ----
+  output$tble <- renderTable({
+    var_name <- str_c("itemid_", input$var)
+    data_table() %>%
+      group_by(get(input$demo)) %>%
+      summarize(
+        n = n(), 
+        `mean of selected measurements` = mean(get(var_name)),
+        `SD of selected measurements` = sd(get(var_name)),
+        `median of selected measurements` = median(get(var_name)),
+        `min of selected measurements` = min(get(var_name)),
+        `max of selected measurements` = max(get(var_name))
+      ) %>%
+      rename(`Selected demographic variable` = "get(input$demo)")
   })
-  
-  output$test2 <- renderText(print(str_c("itemid_", input$var)))
-  
+
 }
 
 # Run the app ----
